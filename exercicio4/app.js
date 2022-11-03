7dcdb0e62d4baa5c2240826e8f21d7a00f5493b2
André Hopf <andre.hopf@gmail.com>
2022-11-03 11:03:03 -0300

    Cria classe Animal e subclasses Gato e Cachorro

---
+++ exercicio4/app.js
@@ -0,0 +1,10 @@
+import { Gato, Cachorro } from './Animal.js';
+
+const marley = new Cachorro('Marley', 5);
+
+const magnolia = new Gato('Magnolia', 1);
+
+console.log(marley);
+console.log(marley.som);
+console.log(magnolia);
+console.log(magnolia.som);
